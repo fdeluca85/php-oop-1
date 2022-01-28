13dd3b96283aee8abd146b87affa3621ea5b937b
francesca <fradeluca85@gmail.com>
2022-01-28 16:49:10 +0100

    costruttore

--- classes/movie.php
+++ classes/movie.php
@@ -3,7 +3,19 @@ class Movie
 {
     public $titolo;
     public $anno;
-    public $prodizione;
+    public $produttore;
     public $regista;
     public $attoreProtagonista;
+    public $genere;
+
+
+    public function __construct($_titolo, $_anno, $_produttore,  $_regista, $_attoreProtagonista, $_genere)
+    {
+        $this->titolo = $_titolo;
+        $this->anno = $_anno;
+        $this->produttore = $_produttore;
+        $this->regista = $_regista;
+        $this->attoreProtagonista = $_attoreProtagonista;
+        $this->genere = $_genere;
+    }
 }
