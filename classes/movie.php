5507e59d8b7eeb0b550c8ae18346ef7ba06aeae8
francesca <fradeluca85@gmail.com>
2022-01-28 17:14:06 +0100

    html

--- classes/movie.php
+++ classes/movie.php
@@ -3,17 +3,15 @@ class Movie
 {
     public $titolo;
     public $anno;
-    public $produttore;
     public $regista;
     public $attoreProtagonista;
     public $genere;
 
 
-    public function __construct($_titolo, $_anno, $_produttore,  $_regista, $_attoreProtagonista, $_genere)
+    public function __construct($_titolo, $_anno, $_regista, $_attoreProtagonista, $_genere)
     {
         $this->titolo = $_titolo;
         $this->anno = $_anno;
-        $this->produttore = $_produttore;
         $this->regista = $_regista;
         $this->attoreProtagonista = $_attoreProtagonista;
         $this->genere = $_genere;
--- index.php
+++ index.php
@@ -1,2 +1,48 @@
 <?php
 require_once __DIR__ . '/classes/movie.php';
+
+$movies = [
+    new Movie("Titanic", 1997, "James Cameron", "Leonardo DiCaprio", "Drammatico"),
+    new Movie("Forrest Gump", 1994, "Robert Zemeckis", "Tom Hanks", "Drammatico"),
+];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Movies</title>
+</head>
+
+<body>
+    <h1>Films</h1>
+    <?php foreach ($movies as $movie) { ?>
+        <div>
+            <h2>
+                <?php echo $movie->titolo ?>
+            </h2>
+            <ul>
+                <li>
+                    <?php echo $movie->anno ?>
+                </li>
+                <li>
+                    <?php echo $movie->regista ?>
+                </li>
+                <li>
+                    <?php echo $movie->attoreProtagonista ?>
+                </li>
+                <li>
+                    <?php echo $movie->genere ?>
+                </li>
+
+            </ul>
+        </div>
+
+
+    <?php } ?>
+
+</body>
+
+</html>
